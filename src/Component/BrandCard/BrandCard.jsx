ac10c8f83821aa11b332720067ad612da41e577f
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-10-18 23:13:49 +0600

    added brand name in home page from the fake data

---
+++ src/Component/BrandCard/BrandCard.jsx
@@ -0,0 +1,21 @@
+const BrandCard = ({ brand }) => {
+  const { name, image } = brand;
+  return (
+    <div className="card bg-base-100 shadow-xl mt-2">
+      <figure>
+        <img className="h-48" src={image} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+
+        <div className="card-actions justify-center">
+          <button className="btn btn-block btn-primary">
+            Show All Prodact
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BrandCard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,21 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../../Component/Banner/Banner";
+import BrandCard from "../../Component/BrandCard/BrandCard";
 
 
 const Home = () => {
+  const brands = useLoaderData();
   return (
     <div>
       <Banner></Banner>
+      <div className="mt-16">
+        <h1 className="text-3xl font-bold text-center">Brand Name</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto py-10">
+          {brands.map((brand) => (
+            <BrandCard key={brand.id} brand={brand}></BrandCard>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
